d4e95ec76c1f3f92ccb16c34655c8ffc6fbbf0fb
shubham2o <shubhamsharma.ha@gmail.com>
2022-06-27 16:48:37 +0530

    Bugs removed and full project completed

--- script.js
+++ script.js
@@ -1,29 +1,28 @@
 const msg = new SpeechSynthesisUtterance();
 let voices = [];
 const voicesDropdown = document.querySelector('[name="voice"]');
-const options = document.querySelector('[type="range", [name = "text"]');
+const options = document.querySelectorAll('[type="range"], [name="text"]');
 const speakButton = document.querySelector('#speak');
 const stopButton = document.querySelector('#stop');
 msg.text = document.querySelector('[name="text"]').value;
 
 function populateVoices() {
     voices = this.getVoices();
-    voicesDropdown.innerHTML =
-        voices
+    voicesDropdown.innerHTML = voices
         .filter(voice => voice.lang.includes('en'))
-        .map(voice => `<option value="${voice.name}">${voice.lang})</option>`)
+        .map(voice => `<option value="${voice.name}">${voice.name} (${voice.lang})</option>`)
         .join('');
 }
 
 function setVoice() {
-    msg.voice = voices.find(voice => voice.name == this.value);
+    msg.voice = voices.find(voice => voice.name === this.value);
     toggle();
 }
 
 function toggle(startOver = true) {
     speechSynthesis.cancel();
     if (startOver) {
-        speechSynthesis.speal(msg);
+        speechSynthesis.speak(msg);
     }
 }
 
@@ -33,8 +32,8 @@ function setOption() {
     toggle();
 }
 
-SpeechSynthesis.addEventListener('voiceschanged', populateVoices);
+speechSynthesis.addEventListener('voiceschanged', populateVoices);
 voicesDropdown.addEventListener('change', setVoice);
-options.foreach(option => option.addEventListener('change', setOption));
+options.forEach(option => option.addEventListener('change', setOption));
 speakButton.addEventListener('click', toggle);
 stopButton.addEventListener('click', () => toggle(false));
